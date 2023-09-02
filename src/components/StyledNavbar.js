bc9b200991b37078b8bb33a7bb284f3df3083696
Rabiu Kabir <reachrabiukabir@gmail.com>
2023-09-02 16:45:53 +0100

    update

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className='navbar'>
+    <nav className="navbar">
       <NavLink
-        to='/'
-        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+        to="/"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         Home
       </NavLink>
       <NavLink
-        to='/about'
-        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+        to="/about"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         About
       </NavLink>
       <NavLink
-        to='/products'
-        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+        to="/products"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         Products
       </NavLink>
       <NavLink
-        to='/login'
-        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+        to="/login"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         Login
       </NavLink>
